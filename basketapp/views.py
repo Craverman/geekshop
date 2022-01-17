bbc1ea3fd5c333aef280a7f25f2a6c5ed0b2b56e
Craverman <CraVerka@googlemail.com>
2022-01-18 02:14:42 +0300

    Lesson 6. Hot product cd /home/vladislavz/Desktop/djangoBasics/geekshop ; /usr/bin/env /home/vladislavz/Desktop/djangoBasics/geekshop/venv/bin/python /home/vladislavz/.vscode/extensions/ms-python.python-2021.12.1559732655/pythonFiles/lib/python/debugpy/launcher 38831 -- /home/vladislavz/Desktop/djangoBasics/geekshop/manage.py runserver 0.0.0.0:8000

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,77 +1,29 @@
-import random
-
-from django.conf import settings
-from django.shortcuts import get_object_or_404, render
-from django.utils import timezone
+from django.shortcuts import HttpResponseRedirect, get_object_or_404, render
 
 from basketapp.models import Basket
-
-from .models import Contact, Product, ProductCategory
-
-
-def main(request):
-    title = "главная"
-
-    products = Product.objects.all()[:4]
-
-    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
-    return render(request, "mainapp/index.html", content)
+from mainapp.models import Product
 
 
-def get_basket(user):
-    if user.is_authenticated:
-        return Basket.objects.filter(user=user)
-    else:
-        return []
+def basket(request):
+    # content = {}
+    # return render(request, "basketapp/basket.html", content)
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
-def get_hot_product():
-    products = Product.objects.all()
-    return random.sample(list(products), 1)[0]
+def basket_add(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    basket = Basket.objects.filter(user=request.user, product=product).first()
+
+    if not basket:
+        basket = Basket(user=request.user, product=product)
+
+    basket.quantity += 1
+    basket.save()
+
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
-def get_same_products(hot_product):
-    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
-    return same_products
-
-
-def products(request, pk=None):
-    title = "продукты"
-    links_menu = ProductCategory.objects.all()
-    basket = get_basket(request.user)
-
-    if pk is not None:
-        if pk == 0:
-            products = Product.objects.all().order_by("price")
-            category = {"name": "все"}
-        else:
-            category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk).order_by("price")
-        content = {
-            "title": title,
-            "links_menu": links_menu,
-            "category": category,
-            "products": products,
-            "media_url": settings.MEDIA_URL,
-            "basket": basket,
-        }
-        return render(request, "mainapp/products_list.html", content)
-    hot_product = get_hot_product()
-    same_products = get_same_products(hot_product)
-    content = {
-        "title": title,
-        "links_menu": links_menu,
-        "same_products": same_products,
-        "media_url": settings.MEDIA_URL,
-        "basket": basket,
-        "hot_product": hot_product,
-    }
-    return render(request, "mainapp/products.html", content)
-
-
-def contact(request):
-    title = "о нас"
-    visit_date = timezone.now()
-    locations = Contact.objects.all()
-    content = {"title": title, "visit_date": visit_date, "locations": locations}
-    return render(request, "mainapp/contact.html", content)
+def basket_remove(request):
+    # content = {}
+    # return render(request, "basketapp/basket.html", content)
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
